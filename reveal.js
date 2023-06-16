6b044eaa69682536bb0ee465b0506316567b7659
warishas <warisha9@hotmail.com>
2023-06-16 11:34:07 -0700

    reveal on scroll

---
+++ reveal.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener('scroll', revealOnScroll);
+
+function revealOnScroll() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 0;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('reveal-visible');
+        }
+    }
+}
